d26415f133e6c37c1a546eda88aeb828959600a8
Владимир Солдатов <cbb110@rambler.ru>
2022-05-12 10:43:43 +0300

    Добавьте файлы проекта.

---
+++ Sort_Insert/Sort_Insert.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+
+using namespace std;
+template <class T>
+void selectSort(T a[], int size) {
+	long i, j, k;
+	T x;
+	// i - номер текущего шага
+	for (i = 0; i < size; i++) {
+		k = i;
+		x = a[i];
+		// цикл выбора наименьшего элемента 
+		for (j = i + 1; j < size; j++)
+			if (a[j] < x) {
+				k = j;
+				x = a[j];
+				// k - индекс наименьшего элемента
+			}
+		if (k != i) {
+			a[k] = a[i];
+			a[i] = x;
+			// меняем местами наименьший с a[i]
+		}
+	}
+}
+
+int main() {
+	srand(time(NULL));
+	const int SIZE = 10;
+	int ar[SIZE];
+	// до сортировки 
+	for (int i = 0; i < SIZE; i++) {
+		ar[i] = rand() % 100;
+		cout << ar[i] << "\t";
+	}
+		cout << "\n\n";
+
+		selectSort(ar, SIZE);
+		// после сортировки
+		for (int i = 0; i < SIZE; i++) {
+			cout << ar[i] << "\t";
+		}
+		cout << "\n\n";
+		return 0;
+	}
